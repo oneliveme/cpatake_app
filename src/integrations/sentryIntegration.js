7492529258f0112e7986764e1626be413336634f
Chris Ruby Jane <53995550+lesyeuxdechris@users.noreply.github.com>
2025-10-24 15:28:53 +0200

    another set of sentry fixes
    i cannot test locally i get nonsense native errors lol

--- src/integrations/sentryIntegration.js
+++ src/integrations/sentryIntegration.js
@@ -1,29 +1,21 @@
+const Sentry = require('@sentry/electron');
 const SENTRY_DSN = 'https://5779734913abdcc9df38156c539ae191@o4508235608555520.ingest.de.sentry.io/4510244448174160';
-
 const APP_VERSION = require('../../package.json').version;
 
-const commonConfig = {
-  dsn: SENTRY_DSN,
-  release: `clubpenguinatake@${APP_VERSION}`,
-  environment: process.env.NODE_ENV || 'production',
-  sampleRate: 1.0,
-  autoSessionTracking: true,
-  debug: false
-};
-
-function initSentryMain() {
-  const { init } = require('@sentry/electron/main');
-  init(commonConfig);
-  console.log('[Sentry] Main process initialized');
-}
-
-function initSentryRenderer() {
-  const { init } = require('@sentry/electron/renderer');
-  init(commonConfig);
-  console.log('[Sentry] Renderer process initialized');
+function initSentry() {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+  
+    release: `clubpenguinatake@${APP_VERSION}`,
+    environment: process.env.NODE_ENV || 'production',
+    sampleRate: 1.0,
+    enableAutoSessionTracking: true,
+    debug: false
+  });
+  
+  console.log('[Sentry] Initialized');
 }
 
 module.exports = {
-  initSentryMain,
-  initSentryRenderer
+  initSentry
 };
